278e1db77d16bab13576142b1315f3f16f3a9baa
AmandaOrr <aorr77@gmail.com>
2023-10-17 18:55:08 -0700

    pushing intial project

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//importing component
+import NavBar from "./NavBar";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <NavBar />
+    </div>
+  )
+}
+
+export default Home;
